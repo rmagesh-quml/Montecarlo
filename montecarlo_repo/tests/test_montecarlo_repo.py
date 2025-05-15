1ab802ac7811400a1ced56f79fa95f6308bbe684
rmagesh-quml <rohanmagesh@gmail.com>
2025-05-14 22:50:30 -0400

    test

--- montecarlo_repo/tests/test_montecarlo_repo.py
+++ montecarlo_repo/tests/test_montecarlo_repo.py
@@ -10,8 +10,18 @@ import numpy as np
 import networkx as nx
 
 import montecarlo_repo as montecarlo
+from montecarlo_repo.BitString_Class import BitString
 
 
 def test_montecarlo_repo_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "montecarlo_repo" in sys.modules
+
+def test_flip_site():
+    b = BitString(3)
+    b.set_config([0, 0, 0])
+    b.flip_site(1)
+    assert b.config.tolist() == [0,1,0]
+    b.flip_site(1)
+    assert b.config.tolist() == [0,0,0]
+
