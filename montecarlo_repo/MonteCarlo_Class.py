f21e07e48127a87a59ed6cb9c7eb30a2580e227b
Rohan Magesh <60275372+rmagesh-quml@users.noreply.github.com>
2025-05-14 22:02:52 -0400

    Update MonteCarlo_Class.py

--- montecarlo_repo/MonteCarlo_Class.py
+++ montecarlo_repo/MonteCarlo_Class.py
@@ -12,26 +12,4 @@ class MonteCarlo:
         self.config = BitString(self.N)
     
     def run(self, T: int, n_samples: int, n_burn: int):
-        beta = 1.0/T
-        E_vals = []
-        M_vals = []
-
-        for step in range(n_samples + n_burn):
-            for site in range(self.N):
-                trial = BitString(self.N)
-                trial.set_config(self.config.config.copy())
-                trial.flip_site(site)
-
-                E_current = self.ham.energy(self.config)
-                E_trial = self.ham.energy(trial)
-                dE = E_trial - E_current
-
-                if dE < 0 or np.random.rand() < np.exp(-beta * dE):
-                    self.config = trial
-
-            if step >= n_burn:
-                E_vals.append(self.ham.energy(self.config))
-                spin_vals = [-1 if x == 0 else 1 for x in self.config.config]
-                M_vals.append(sum(spin_vals))
-
-        return np.array(E_vals), np.array(M_vals)
+        pass
