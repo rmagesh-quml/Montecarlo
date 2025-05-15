e200bb865049d89b68ab9315dd058826abbe282e
rmagesh-quml <rohanmagesh@gmail.com>
2025-05-14 23:17:33 -0400

    Add more tests

--- montecarlo_repo/tests/test_montecarlo_repo.py
+++ montecarlo_repo/tests/test_montecarlo_repo.py
@@ -25,3 +25,22 @@ def test_flip_site():
     b.flip_site(1)
     assert b.config.tolist() == [0,0,0]
 
+def test_Ising():
+    N = 6
+    Jval = 2.0
+    G = nx.Graph()
+    G.add_nodes_from([i for i in range(N)])
+    G.add_edges_from([(i,(i+1)% G.number_of_nodes() ) for i in range(N)])
+    for e in G.edges:
+        G.edges[e]['weight'] = Jval
+    
+    conf = montecarlo.BitString(N)
+    ham = montecarlo.IsingHamiltonian(G)
+    ham.set_mu(np.array([.1 for i in range(N)]))
+    conf.flip_site(2)
+    conf.flip_site(3)
+    print(conf)
+    e = ham.energy(conf)
+    print(e)
+    assert(np.isclose(e, 3.8))
+
